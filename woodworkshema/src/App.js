cb6300fbd71d95148b1b5f4f48721c9676d72463
nduwimanaguy <guylan@live.nl>
2023-07-01 16:09:29 +0000

    read and edit and download link

--- woodworkshema/src/App.js
+++ woodworkshema/src/App.js
@@ -1,37 +1,39 @@
 import React from 'react';
 import * as XLSX from 'xlsx';
 import FileSaver from 'file-saver';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  const handleDownloadExcel = () => {
-    /* Read the existing workbook */
-    const workbook = XLSX.readFile('xfactuurtemplate.xlsx');
-    /* Get the first worksheet */
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    /* Modify the worksheet as needed */
-    worksheet.A1.v = 'Hello, world!';
-    /* Write the modified workbook back to the file */
-    XLSX.writeFile(workbook, '/public/xfactuurtemplate.xlsx');
+  const handleEditExcel = () => {
+    // Read the Excel file from the public/ directory
+    const excelFilePath = process.env.PUBLIC_URL + '/xfactuurtemplate.xlsx';
+
+    fetch(excelFilePath)
+      .then((response) => response.arrayBuffer())
+      .then((data) => {
+        /* Read the existing workbook */
+        const wb = XLSX.read(data, { type: 'array' });
+
+        /* Get the first worksheet from the workbook */
+        const ws = wb.Sheets[wb.SheetNames[0]];
+
+        /* Edit the value in cell A1 */
+        XLSX.utils.sheet_add_aoa(ws, [['Hello, world! (edited)']], { origin: 'A1' });
+
+        /* Write the updated workbook to a file */
+        const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+        const blob = new Blob([wbout], { type: 'application/octet-stream' });
+        FileSaver.saveAs(blob, 'xfactuurtemplate_edited.xlsx');
+      })
+      .catch((error) => {
+        console.error('Error reading the file:', error);
+      });
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={handleDownloadExcel}>Download Excel</button>
+        {/* Your app UI */}
+        <button onClick={handleEditExcel}>Edit Excel</button>
       </header>
     </div>
   );
